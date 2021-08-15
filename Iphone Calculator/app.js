1c58ca1081d2bb8eea2fce8cf347ecb324752512
HanadSalim <45672173+HanadSalim@users.noreply.github.com>
2021-08-15 23:11:18 +0400

    displays numbers, and decimal point correctly. AC works as well.

--- Iphone Calculator/app.js
+++ Iphone Calculator/app.js
@@ -2,10 +2,10 @@
 const numEl = document.getElementsByClassName("number");
 const funcEl = document.getElementsByClassName("function");
 const decEl = document.querySelector(".decimal");
+const screen = document.querySelector(".screen");
+var point = false;
 
-
-
-//Event listener
+//Number EventListener
 for(let i=0; i<numEl.length; i++){
     const num = numEl[i];
     num.addEventListener('click', () => {
@@ -14,5 +14,39 @@ for(let i=0; i<numEl.length; i++){
 }
 
 const numclick = (e) => {
-    console.log(e);
+    if(screen.innerHTML=="0"){
+        screen.innerHTML=e;
+    }else{
+        screen.innerHTML=screen.innerHTML+e;
+    }
 }
+
+//Decimal EventListener
+decEl.addEventListener('click', () => {
+    if (point == false)
+    {
+        screen.innerHTML=screen.innerHTML+".";
+        point = true;
+    }
+})
+
+//Function event listener
+for(let i=0; i<funcEl.length; i++){
+    const func = funcEl[i];
+    func.addEventListener('click', () => {
+        funcclick(func);
+    })
+}
+
+const funcclick = (func) => {
+    if(func.innerHTML=="AC"){
+        screen.innerHTML=0;
+        point = false;
+    }
+    // if(func.innerHTML=="C"){
+    //     screen.innerHTML=0;
+    // }
+}
+
+
+
