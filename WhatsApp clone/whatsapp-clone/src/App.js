0e12bc1767afff801bc84cfc59dc34e6b930d6ce
HanadSalim <45672173+HanadSalim@users.noreply.github.com>
2021-10-31 23:27:28 +0000

    Started to create the app body

---
+++ WhatsApp clone/whatsapp-clone/src/App.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import Sidebar from './Sidebar';
+import './App.css';
+
+
+function App() {
+  return (
+    <div className="app">
+      <div className="app__body">
+        <Sidebar />
+        {/*  chat */}
+      </div>
+    </div>
+  );
+}
+
+export default App;
---
+++ WhatsApp clone/whatsapp-clone/src/firebase.js
@@ -0,0 +1,8 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyC3vIsTEXFPdPHpaq902SegISlVfm4NB2Q",
+    authDomain: "whatsapp-clone-51311.firebaseapp.com",
+    projectId: "whatsapp-clone-51311",
+    storageBucket: "whatsapp-clone-51311.appspot.com",
+    messagingSenderId: "474220246334",
+    appId: "1:474220246334:web:9ce05cb0fcf73a7057e86f"
+  };
