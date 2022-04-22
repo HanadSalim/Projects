8fded14d6db76c74d6a5249b30a58ff11cb1f06b
HanadSalim <hanad.s@hotmail.com>
2022-04-22 20:26:27 +0100

    improving UI

--- WhatsApp clone/whatsapp-clone/src/Welcome.js
+++ WhatsApp clone/whatsapp-clone/src/Welcome.js
@@ -1,12 +1,30 @@
 import React, { useContext } from 'react'
 import Chat from './Chat'
 import { RoomContext } from './context/roomContext'
-
+import './Welcome.css'
 function Welcome(){
     const [context, setContext] = useContext(RoomContext)
     const {selected} = context 
     return(selected?
-        <Chat />:<div className='chat'>welcome message</div>
+        <Chat />:
+        <div className='wc'>
+            <div className='wrapper'>
+                <div className='pc'>
+                    <div className='image'></div>
+                </div>
+                <div className='tc'>
+                    <div className='heading'>
+                        <h1>WhatsApp Web</h1>
+                        <div><b>NEW</b></div>
+                    </div>
+                    <div className='text'>
+                    Now send and receive messages without keeping your phone online.<br/>
+                    Use WhatsApp on up to 4 linked devices and 1 phone at the same time.
+                    </div>
+                </div>
+                
+            </div>
+        </div>
     )
 }
 
--- WhatsApp clone/whatsapp-clone/src/firebase.js
+++ WhatsApp clone/whatsapp-clone/src/firebase.js
@@ -1,6 +1,8 @@
 import {initializeApp} from "firebase/app";
 import { getFirestore } from "@firebase/firestore";
 import {getAuth,GoogleAuthProvider,signInWithPopup} from "firebase/auth"
+
+
 const firebaseConfig = {
     apiKey: "AIzaSyC3vIsTEXFPdPHpaq902SegISlVfm4NB2Q",
     authDomain: "whatsapp-clone-51311.firebaseapp.com",
@@ -8,17 +10,18 @@ const firebaseConfig = {
     storageBucket: "whatsapp-clone-51311.appspot.com",
     messagingSenderId: "474220246334",
     appId: "1:474220246334:web:9ce05cb0fcf73a7057e86f"
-  };
+  }
 
+//  initialise firebase
+initializeApp(firebaseConfig)
 
-  //  initialise firebase
-const app = initializeApp(firebaseConfig);
-export default getFirestore();
+export default getFirestore()
 
 export const auth = getAuth() 
+
 const provider = new GoogleAuthProvider()
+
 export const signInWithgoogle = () => {
-  // window.open("localhost:3000","","toolbar=0,location=0,menubar=1,scrollbars=1,resizable=1")
   signInWithPopup(auth,provider).then((result)=>{
     console.log(result)
   }).catch((error)=>console.log(error))
